5682120d87f3a0f34a02142db3c0ddb9b3de9f85
StevenDessasore <stevendessasoreba@gmail.com>
2021-08-17 01:40:16 -0400

    commit tree 7bf1cd39db347148d2c2f7de80e4e6bcdfb04625

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end
+
+    get '/name' do
+        "My name is Steven"
+    end
+
+    get '/hometown' do
+        "My hometown is The Bronx"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is '500 horses' by Nipsey Hussle"
+    end
 end
